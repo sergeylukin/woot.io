f9d566cb571489f24104065e046a143475993fba
Sergey Lukin <contact@sergeylukin.com>
2022-06-04 16:33:00 +0300

    graphql: auto generate schema

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,6 +1,6 @@
 import { ApolloServer } from "apollo-server-micro";
 
-import { typeDefs } from "../../graphql/schema";
+import { schema } from "../../graphql/schema";
 import { resolvers } from "../../graphql/resolvers";
 import { createContext } from "../../graphql/context";
 import Cors from "micro-cors";
@@ -8,7 +8,7 @@ import Cors from "micro-cors";
 const cors = Cors();
 
 const apolloServer = new ApolloServer({
-  typeDefs,
+  schema,
   resolvers,
   context: createContext,
 });
