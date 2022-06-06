ab211249e4e0ca498c477c204642da9c1560067f
Sergey Lukin <contact@sergeylukin.com>
2022-06-06 04:36:15 +0300

    remove non typed resolvers

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,7 +1,6 @@
 import { ApolloServer } from "apollo-server-micro";
 
 import { schema } from "../../graphql/schema";
-import { resolvers } from "../../graphql/resolvers";
 import { createContext } from "../../graphql/context";
 import Cors from "micro-cors";
 
@@ -9,7 +8,6 @@ const cors = Cors();
 
 const apolloServer = new ApolloServer({
   schema,
-  resolvers,
   context: createContext,
 });
 
